95b1924c0b9d3ba4c891d79e1813b7b1e8c84862
XRay3D <xray3d@ya.ru>
2021-10-10 23:03:12 +0300

    Причесал SvdNode

--- svdnode.h
+++ svdnode.h
@@ -7,20 +7,26 @@ class SvdNode {
     friend class SvdParser;
 
 public:
-    explicit SvdNode(const QList<QVariant>& data, SvdNode* parentItem = 0);
+    explicit SvdNode(const QList<QVariant>& data, SvdNode* parentItem = nullptr);
+
     ~SvdNode();
 
-    void appendChild(SvdNode* child);
+    QVariant data(int column) const { return data_.value(column); }
 
-    SvdNode* child(int row);
-    int childCount() const;
-    int columnCount() const;
-    QVariant data(int column) const;
-    int row() const;
-    SvdNode* parent();
+    SvdNode* child(int row) { return childs_.value(row); }
+
+    SvdNode* parent() { return parent_; }
+
+    int childCount() const { return childs_.count(); }
+
+    int columnCount() const { return data_.count(); }
+
+    int row() const { return parent_ ? parent_->childs_.indexOf(const_cast<SvdNode*>(this)) : 0; }
+
+    void appendChild(SvdNode* item) { childs_.append(item); }
 
 private:
-    QList<SvdNode*> m_childItems;
     QList<QVariant> data_;
-    SvdNode* m_parentItem;
+    QList<SvdNode*> childs_;
+    SvdNode* const parent_;
 };
